a8ae77d90507933219c76d5ae4f7ef33a828dd1a
danRolon <daniel.rolon@yahoo.com>
2019-08-01 21:36:33 -0500

    trying to add roles

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 const server = require('./api/server.js');
 
 // make dynamic
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`Server is listening on port ${port}`));
